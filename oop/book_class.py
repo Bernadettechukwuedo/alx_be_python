d704fe587295b929818113fede0c0f7d52792a36
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2025-01-24 13:47:51 +0100

    updated the code

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title:str, author:str, year:int):
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
         self.year = year
@@ -11,4 +11,4 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book({self.title}, {self.author}, {self.year})"
+        return f"Book('{self.title}', '{self.author}', '{self.year}')"
